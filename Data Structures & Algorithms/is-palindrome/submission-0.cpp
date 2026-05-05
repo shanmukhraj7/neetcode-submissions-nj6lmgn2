21fa5e697e1a0a7ed482934c78feb4c2502bb5f5
shanmukhraj7 <148477598+shanmukhraj7@users.noreply.github.com>
2026-05-05 16:57:09 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string t = "";
+        for(auto c : s){
+            if(isalnum(c)){
+                t += tolower(c);
+            }
+        }
+        int l = 0, r = t.length() - 1;
+        while(l <= r){
+            if(t[l] != t[r])
+                return false;
+            l++, r--;
+        }
+        return true;
+    }
+};
